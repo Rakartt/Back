6eb7da0157d74dc6961c68e428a815174998d243
sebastian <sebastia061599@gmail.com>
2024-09-16 21:07:39 +0000

    creating backend

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from datetime import datetime
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://ec2-98-80-13-141.compute-1.amazonaws.com"}})
+
+# Configuración de la base de datos
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mi_usuario:12345@localhost/registro_usuarios'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Definir el modelo de la tabla Usuario
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombres = db.Column(db.String(100), nullable=False)
+    apellidos = db.Column(db.String(100), nullable=False)
+    fecha_nacimiento = db.Column(db.Date, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, nombres, apellidos, fecha_nacimiento, password):
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.fecha_nacimiento = fecha_nacimiento
+        self.password = password
+
+# Crear la base de datos y las tablas
+with app.app_context():
+    db.create_all()
+
+@app.route('/usuarios', methods=['POST'])
+def registrar_usuario():
+    data = request.get_json()
+    nuevo_usuario = Usuario(
+        nombres=data['nombres'],
+        apellidos=data['apellidos'],
+        fecha_nacimiento=datetime.strptime(data['fecha_nacimiento'], '%Y-%m-%d'),
+        password=data['password']
+    )
+    db.session.add(nuevo_usuario)
+    db.session.commit()
+    return jsonify({"message": "Usuario registrado exitosamente"}), 201
+
+@app.route('/registro', methods=['GET'])
+def obtener_usuarios():
+    usuarios = Usuario.query.all()
+    resultado = [
+        {"nombres": usuario.nombres, "apellidos": usuario.apellidos, "fecha_nacimiento": usuario.fecha_nacimiento}
+        for usuario in usuarios
+    ]
+    return jsonify(resultado)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
